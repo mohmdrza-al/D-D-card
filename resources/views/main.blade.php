4cdceb814fe274e49b84c9917f7e2225b54168c4
mohmdrza <m.alaghebandshz@gmail.com>
2024-05-29 20:27:24 +0330

    add new view template and package-lock.json

---
+++ resources/views/main.blade.php
@@ -0,0 +1,31 @@
+<div id="page">
+    <div class="card">
+        <div class="half half-left">
+            <div class="img-container">
+                <img src="https://images.unsplash.com/photo-1481578279695-5864d6d6ff2a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7cafa2bd02d15824d147667f8e72b090&auto=format&fit=crop&w=2134&q=80" alt="">
+            </div>
+        </div>
+        <div class="half half-right">
+            <a href="" class="online">online</a>
+            <h2 class="name">Mary Jane</h2>
+            <p>(Singer, Model)</p>
+            <h3 class="bio">Bio</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit ligula nisl penatibus purus, habitasse varius sociis cras malesuada lacus bibendum montes rhoncus et primis non, odio augue potenti hendrerit litora eros vulputate fames dui cursus. Tincidunt et litora ligula dictumst rutrum class viverra vestibulum, maecenas metus leo facilisis a lobortis erat hendrerit, aptent nibh inceptos cras ornare at blandit. Orci commodo facilisi facilisis nisl et per duis dictumst pretium nascetur dui tempor.</p>
+            <h3 class="location">Location</h3>
+            <p>Bucharest, Romania</p>
+            <button>Message</button>
+        </div>
+        <div class="triangle">
+            <ul>
+                <li><a href="#" class="fa fa-facebook"></a></li>
+                <li><a href="#" class="fa fa-twitter"></a></li>
+                <li><a href="#" class="fa fa-skype"></a></li>
+                <li><a href="#" class="fa fa-youtube-play"></a></li>
+                <li><a href="#" class="fa fa-instagram"></a></li>
+                <li><a href="#" class="fa fa-snapchat-ghost"></a></li>
+            </ul>
+            <span>Member Since 2015</span>
+        </div>
+        <a href="#" class="fa fa-plus"></a>
+    </div>
+</div>
