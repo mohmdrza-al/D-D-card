b2f976f291daf286882c0145436f0460216b4c74
mohmdrza <m.alaghebandshz@gmail.com>
2024-05-29 15:33:07 -0700

    fix css and js issue

---
+++ resources/js/script.js
@@ -0,0 +1,2 @@
+//Image https://unsplash.com/photos/nahUo1GhcrA
+//Inpired by https://dribbble.com/shots/3525050-Profile-Card
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,8 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mohammadreza Alagheband</title>
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css'>
+    <link  href="https://wizardly-babbage-jojsr-tde.storage.iran.liara.space/style.css" rel='stylesheet'>
+</head>
+<body>
+<!-- partial:index.partial.html -->
 <div id="page">
     <div class="card">
         <div class="half half-left">
             <div class="img-container">
-                <img src="https://images.unsplash.com/photo-1481578279695-5864d6d6ff2a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7cafa2bd02d15824d147667f8e72b090&auto=format&fit=crop&w=2134&q=80" alt="">
+                <img
+                    src="https://images.unsplash.com/photo-1481578279695-5864d6d6ff2a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7cafa2bd02d15824d147667f8e72b090&auto=format&fit=crop&w=2134&q=80"
+                    alt="">
             </div>
         </div>
         <div class="half half-right">
@@ -10,7 +23,11 @@
             <h2 class="name">Mary Jane</h2>
             <p>(Singer, Model)</p>
             <h3 class="bio">Bio</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit ligula nisl penatibus purus, habitasse varius sociis cras malesuada lacus bibendum montes rhoncus et primis non, odio augue potenti hendrerit litora eros vulputate fames dui cursus. Tincidunt et litora ligula dictumst rutrum class viverra vestibulum, maecenas metus leo facilisis a lobortis erat hendrerit, aptent nibh inceptos cras ornare at blandit. Orci commodo facilisi facilisis nisl et per duis dictumst pretium nascetur dui tempor.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit ligula nisl penatibus purus, habitasse varius
+                sociis cras malesuada lacus bibendum montes rhoncus et primis non, odio augue potenti hendrerit litora
+                eros vulputate fames dui cursus. Tincidunt et litora ligula dictumst rutrum class viverra vestibulum,
+                maecenas metus leo facilisis a lobortis erat hendrerit, aptent nibh inceptos cras ornare at blandit.
+                Orci commodo facilisi facilisis nisl et per duis dictumst pretium nascetur dui tempor.</p>
             <h3 class="location">Location</h3>
             <p>Bucharest, Romania</p>
             <button>Message</button>
@@ -29,3 +46,8 @@
         <a href="#" class="fa fa-plus"></a>
     </div>
 </div>
+<!-- partial -->
+<script src="{{ asset('js/script.js') }}"></script>
+
+</body>
+</html>
